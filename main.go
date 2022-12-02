6bdf03e5d2bb5f09e9baaad43c9fdde3b059987b
Jim Bergstrom <james.bergstrom@fastmail.com>
2022-12-01 20:49:36 -0700

    Display words in a grid

--- main.go
+++ main.go
@@ -15,15 +15,21 @@ import (
 var wordList string
 
 func main() {
-	numWords := flag.Int("n", 10, "Number of words to generate")
+	numWords := flag.Int("n", 12, "Number of words to generate")
 	flag.Parse()
 	words := loadWords()
-	for i := 0; i < *numWords; i++ {
+	for i := 1; i <= *numWords; i++ {
 		word, err := randWord(words)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(word)
+		fmt.Printf("%-20s", word)
+		if i%3 == 0 {
+			fmt.Print("\n")
+		}
+	}
+	if *numWords%3 != 0 {
+		fmt.Print("\n")
 	}
 }
 
